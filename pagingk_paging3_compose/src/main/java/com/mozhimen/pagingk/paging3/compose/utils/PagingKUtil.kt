32a760c33a6ece83b533042943131bb85c19fea7
Kolin Zhao & Mozhimen <mozhimen@163.com>
2024-10-16 21:30:51 +0800

    1016

--- pagingk_paging3_compose/src/main/java/com/mozhimen/pagingk/paging3/compose/utils/PagingKUtil.kt
+++ pagingk_paging3_compose/src/main/java/com/mozhimen/pagingk/paging3/compose/utils/PagingKUtil.kt
@@ -3,8 +3,13 @@ package com.mozhimen.pagingk.paging3.compose.utils
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.paging.compose.LazyPagingItems
+import kotlinx.coroutines.launch
 
 /**
  * @ClassName PagingKUtil
@@ -24,17 +29,47 @@ fun <T : Any> LazyPagingItems<T>.rememberLazyGridState(): LazyGridState =
 //////////////////////////////////////////////////////////////////////////
 
 object PagingKUtil {
-    @JvmStatic
+//    @JvmStatic
+//    @Composable
+//    fun <T : Any> rememberLazyListState(lazyPagingItems: LazyPagingItems<T>): LazyListState {
+//        // After recreation, LazyPagingItems first return 0 items, then the cached items.
+//        // This behavior/issue is resetting the LazyListState scroll position.
+//        // Below is a workaround. More info: https://issuetracker.google.com/issues/177245496.
+//        return when (lazyPagingItems.itemCount) {
+//            // Return a different LazyListState instance.
+//            0 -> remember(this) { LazyListState(0, 0) }
+//            // Return rememberLazyListState (normal case).
+//            else -> androidx.compose.foundation.lazy.rememberLazyListState()
+//        }
+//    }
+
     @Composable
-    fun <T : Any> rememberLazyListState(lazyPagingItems: LazyPagingItems<T>): LazyListState {
-        // After recreation, LazyPagingItems first return 0 items, then the cached items.
-        // This behavior/issue is resetting the LazyListState scroll position.
-        // Below is a workaround. More info: https://issuetracker.google.com/issues/177245496.
+    @JvmStatic
+    fun <T : Any> /*LazyPagingItems<T>.*/rememberLazyListState(lazyPagingItems: LazyPagingItems<T>): LazyListState {
+        val state = androidx.compose.foundation.lazy.rememberLazyListState()
+        val scrollIndex = rememberSaveable { mutableIntStateOf(0) }
+        val coroutineScope = rememberCoroutineScope()
+        // 重新创建后，LazyPagingItems 首先返回 0 个项目，然后返回缓存的项目。
+        //此行为/问题是重置 LazyListState 滚动位置。
         return when (lazyPagingItems.itemCount) {
-            // Return a different LazyListState instance.
-            0 -> remember(this) { LazyListState(0, 0) }
-            // Return rememberLazyListState (normal case).
-            else -> androidx.compose.foundation.lazy.rememberLazyListState()
+            // 返回不同的 LazyListState 实例
+            0 -> rememberSaveable(saver = LazyListState.Saver) {
+                scrollIndex.intValue = state.firstVisibleItemIndex
+                LazyListState(state.firstVisibleItemIndex, state.firstVisibleItemScrollOffset)
+            }
+
+            else -> {
+                if (scrollIndex.intValue in 1..<lazyPagingItems.itemCount) {
+                    // 滚动后返回状态。
+                    LaunchedEffect("") {
+                        coroutineScope.launch {
+                            state.animateScrollToItem(scrollIndex.intValue, state.firstVisibleItemScrollOffset)
+                            scrollIndex.intValue = 0
+                        }
+                    }
+                }
+                state
+            }
         }
     }
 
@@ -44,11 +79,37 @@ object PagingKUtil {
         // After recreation, LazyPagingItems first return 0 items, then the cached items.
         // This behavior/issue is resetting the LazyListState scroll position.
         // Below is a workaround. More info: https://issuetracker.google.com/issues/177245496.
+//        return when (lazyPagingItems.itemCount) {
+//            // Return a different LazyListState instance.
+//            0 -> remember(this) { LazyGridState(0, 0) }
+//            // Return rememberLazyListState (normal case).
+//            else -> androidx.compose.foundation.lazy.grid.rememberLazyGridState()
+//        }
+
+        val state = androidx.compose.foundation.lazy.grid.rememberLazyGridState()
+        val scrollIndex = rememberSaveable { mutableIntStateOf(0) }
+        val coroutineScope = rememberCoroutineScope()
+        // 重新创建后，LazyPagingItems 首先返回 0 个项目，然后返回缓存的项目。
+        //此行为/问题是重置 LazyListState 滚动位置。
         return when (lazyPagingItems.itemCount) {
-            // Return a different LazyListState instance.
-            0 -> remember(this) { LazyGridState(0, 0) }
-            // Return rememberLazyListState (normal case).
-            else -> androidx.compose.foundation.lazy.grid.rememberLazyGridState()
+            // 返回不同的 LazyListState 实例
+            0 -> rememberSaveable(saver = LazyGridState.Saver) {
+                scrollIndex.intValue = state.firstVisibleItemIndex
+                LazyGridState(state.firstVisibleItemIndex, state.firstVisibleItemScrollOffset)
+            }
+
+            else -> {
+                if (scrollIndex.intValue in 1..<lazyPagingItems.itemCount) {
+                    // 滚动后返回状态。
+                    LaunchedEffect("") {
+                        coroutineScope.launch {
+                            state.animateScrollToItem(scrollIndex.intValue, state.firstVisibleItemScrollOffset)
+                            scrollIndex.intValue = 0
+                        }
+                    }
+                }
+                state
+            }
         }
     }
 }
