11df3f3ed5dec4dc43d4d7a28ff989d65e0cf1be
Kolin Zhao & Mozhimen <mozhimen@163.com>
2025-04-02 20:06:05 +0800

    250402

--- paging3/data/src/main/java/com/mozhimen/pagingk/paging3/data/bases/BasePagingKDataAdapter.kt
+++ paging3/data/src/main/java/com/mozhimen/pagingk/paging3/data/bases/BasePagingKDataAdapter.kt
@@ -3,6 +3,8 @@ package com.mozhimen.pagingk.paging3.data.bases
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.mozhimen.kotlin.elemk.commons.IA_BListener
+import com.mozhimen.kotlin.utilk.kotlin.collections.indexOf
 
 /**
  * @ClassName BasePagedListAdapter
@@ -15,6 +17,18 @@ abstract class BasePagingKDataAdapter<T : Any, VH : RecyclerView.ViewHolder>(ite
     fun getData(position: Int): T? =
         getItem(position)
 
-    fun getDatas():List<T> =
+    fun getDatas(): List<T> =
         snapshot().items
+
+    fun filter(predicate: IA_BListener<T, Boolean>): List<T> =
+        getDatas().filter(predicate)
+
+    fun find(predicate: IA_BListener<T, Boolean>): T? =
+        getDatas().find(predicate)
+
+    fun indexOf(predicate: IA_BListener<T, Boolean>): List<Int> =
+        getDatas().indexOf(predicate)
+
+    fun indexOfFirst(predicate: IA_BListener<T, Boolean>): Int =
+        getDatas().indexOfFirst(predicate)
 }
